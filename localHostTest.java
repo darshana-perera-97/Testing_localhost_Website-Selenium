8582cd9f77fad375e1760be7504f8194a23d66f1
Darshana Perera <darshana.saluka.10@gmail.com>
2022-10-10 14:48:19 +0530

    localhost open project is done

--- localHostTest.java
+++ localHostTest.java
@@ -8,6 +8,9 @@ public class localHostTest {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
+		//local host open website
+		driver.navigate().to("http://localhost/employee/admin/index.php");
 
 	}
 
